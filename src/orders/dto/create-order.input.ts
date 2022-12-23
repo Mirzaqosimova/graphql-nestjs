9104c428df358c0c03e3e8f34f39ee06295d4de4
Farzona Mirzaqosimova <mirzaqosimovafarzona@gmail.com>
2022-12-23 11:44:33 +0500

    feat order find one fixed

--- src/orders/dto/create-order.input.ts
+++ src/orders/dto/create-order.input.ts
@@ -3,5 +3,14 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 @InputType()
 export class CreateOrderInput {
   @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  product_id: number; 
+  
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  user_id: number;  
+  
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  amount: number;
+  
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  price: number;
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectKnex, Knex } from 'nestjs-knex';
 import { CreateOrderInput } from './dto/create-order.input';
 import { UpdateOrderInput } from './dto/update-order.input';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderInput: CreateOrderInput) {
-    return 'This action adds a new order';
+
+  constructor(@InjectKnex() private readonly knex: Knex) {}
+  //validation not implemented
+  async create(createOrderInput: CreateOrderInput) {
+    await this.knex('orders')
+    .insert({
+      ...createOrderInput
+    })
+    ;
+   // return this.findOne()
   }
 
   findAll() {
     return `This action returns all orders`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: number): Promise<Order> {
+    return  this.knex('orders')
+    .where({'orders.id': id})
+    .leftJoin('products',{'products.id':'orders.product_id'})
+    .leftJoin('users',{'users.id':'orders.user_id'})
+    .select(
+      this.knex.raw(`orders.*, to_json(products.*) as product,to_json(users.*) as user `)
+    )
+    .first()
+    .then((item: any)=>{const data: Order = item
+    return data })
   }
 
   update(id: number, updateOrderInput: UpdateOrderInput) {
